37a6a412fe8c24d78f333fe73bf633c1a84402cf
selman çalışkan <selman.clskn@hotmail.com>
2021-02-04 00:09:48 +0300

    update

--- DataAccess/Abstract/ICategoryDal.cs
+++ DataAccess/Abstract/ICategoryDal.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface IProductDal:IEntitiyRepository<Product>
+    public interface ICategoryDal:IEntitiyRepository<Category>
     {
         
     }
