99b215c0acb3c1c276e22680cbfb88fe23b2fd40
selman çalışkan <selman.clskn@hotmail.com>
2021-02-25 00:05:01 +0300

    update

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,6 +10,9 @@ namespace Business.Constants
         public static string ProductAdded = "Ürün eklendi";
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
         public static string MaintenanceTime = "Sistem bakımda";
-        public static string ProductListed = "Ürünler listelendi";        
+        public static string ProductListed = "Ürünler listelendi";
+        public static string ProductCountOfCategoryError = "Bir kategoride en fazla 10 ürün olabilir";
+        public static string ProductNameAlreadyExists = "Böyle bir ürün ismi zaten var";
+        public static string CategoryLimitExceded = "Kategori limiti aşıldığı için yeni ürün eklenemiyor";
     }
 }
